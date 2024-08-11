88970c1a6452cae25d15ccba35d1df52d98d67ee
Pantae <pantae35872@gmail.com>
2024-08-11 13:45:05 +0700

    Fix identaion

--- common/src/constants.rs
+++ common/src/constants.rs
@@ -2,11 +2,13 @@
 pub const MOV_OPCODE: u16 = 16;
 pub const PUSH_OPCODE: u16 = 17;
 pub const POP_OPCODE: u16 = 18;
+
 //Arithmetic instructions
 pub const INC_OPCODE: u16 = 30;
 pub const CMP_OPCODE: u16 = 31;
 pub const ADD_OPCODE: u16 = 32;
 pub const SUB_OPCODE: u16 = 33;
+
 //Branching instructions
 pub const JMP_OPCODE: u16 = 64;
 pub const JMZ_OPCODE: u16 = 65;
@@ -17,8 +19,11 @@ pub const JACC_OPCODE: u16 = 69;
 pub const JACE_OPCODE: u16 = 70;
 pub const JME_OPCODE: u16 = 71;
 pub const JMC_OPCODE: u16 = 72;
+
 //Cpu state releate instructions
 pub const HALT_OPCODE: u16 = 65535;
+
+//Mov sub instructions
 pub const MOV_REG2REG: u8 = 1;
 pub const MOV_REG2MEM: u8 = 2;
 pub const MOV_NUM2REG: u8 = 3;
